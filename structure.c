3a51e12592b1aad384435d23b6bfb3559e221510
charmelle5 <nanacharmelle77@gmail.com>
2025-12-15 10:34:31 +0100

    Update structure.c

--- structure.c
+++ structure.c
@@ -1,16 +1,14 @@
 #define MAX 100
-typedef struct avl{
-    int id;
-    float vol;
-    struct arbre* fg;
-    struct arbre* fd;
-    int eq;
-}AVL;
 
 typedef struct usine{
     char id[MAX];
     int capacite_max;       
     int volume_total; 
-    struct Usine *fg;
-    struct Usine *fd;
 } Usine;
+
+typedef struct avl{
+    Usine usine;
+    struct arbre* fg;
+    struct arbre* fd;
+    int equilibre;
+}AVL;
