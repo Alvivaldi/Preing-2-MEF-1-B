ce22fa3fa31b938c43b251b289164c511253d320
charmelle5 <nanacharmelle77@gmail.com>
2025-12-16 13:15:32 +0100

    Update myScript.sh

--- myScript.sh
+++ myScript.sh
@@ -1,14 +1,1 @@
-jdcbjsdj
-bonjour vous
-hbcksdbkvs
-kjflkndkld
-git config --global user.name "TonNom"
-git config --global user.email "tonemail@example.com"
 
-
-khdjuedhnjishdjdujd
-coucou tout le monde
-oui oaui oui non non oui 
-
-
-ALLER
