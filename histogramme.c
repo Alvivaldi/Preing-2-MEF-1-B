b5def4f6b82333344cabd4e0fe5989e865496727
Lasugaa <lasugaa.k@gmail.com>
2025-12-19 08:58:00 +0000

    histo

---
+++ histogramme.c
@@ -0,0 +1,13 @@
+void remplir_fichiers(AVL a, FILE* Fmax, FILE* Fcapt, FILE* Freel){
+    if(a == NULL){
+         return;
+    }
+
+    remplir_fichiers(a->fd, Fmax, Fcapt, Freel);
+
+    fprintf(Fmax,  "%s;%.2f\n", a->id, a->usine->volume_max);
+    fprintf(Fcapt,  "%s;%.2f\n", a->id, a->usine->volume_capte);
+    fprintf(Freel, "%s;%.2f\n", a->id, a->usine->volume_reel);
+
+    remplir_ficher(AVL a, Fmax, Fcapt, Freel);
+})
