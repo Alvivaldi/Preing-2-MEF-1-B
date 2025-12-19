bb923ea00ff57748ed7f2938066a03f321a21ffa
Lasugaa <lasugaa.k@gmail.com>
2025-12-19 21:52:19 +0000

    fonction ficheier perte

--- fuites.c
+++ fuites.c
@@ -1,1 +1,8 @@
-
+void fichier_pertes(char* id, float pertes){
+    FILE* f=fopen("rendements.dat","a");
+    if (f==NULL){
+        exit(1);
+    }
+    fprintf(f,"%s;%lf\n",id,pertes);
+    fclose(f);
+}
