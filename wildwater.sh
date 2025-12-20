4f3ffdc660a4555b5c0d4868a48b1af51c9cd327
Lasugaa <lasugaa.k@gmail.com>
2025-12-20 23:37:35 +0000

    ajout shell

--- wildwater.sh
+++ wildwater.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+set -e
 
+# Mesure du temps
+START=$(date +%s)
+
+# Affichage du temps total
+END=$(date +%s)
+DURATION=$((END - START))
+
+echo "Durée totale du script : ${DURATION} secondes"
