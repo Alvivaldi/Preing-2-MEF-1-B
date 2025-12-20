9f12406bd945b85c3cacaca2e321603e5aabb743
Lasugaa <lasugaa.k@gmail.com>
2025-12-20 12:16:59 +0000

    deplacer

--- histogramme.c
+++ histogramme.c
@@ -15,3 +15,20 @@ void remplir_fichiers(AVL a, FILE* Fmax, FILE* Fcapt, FILE* Freel){
 
     remplir_ficher(AVL a, Fmax, Fcapt, Freel);
 })
+
+void generer_fichiers_histogramme(AVL a){
+    FILE *Fmax  = fopen("vol_max.dat", "w");
+    FILE *Fcapt  = fopen("vol_captation.dat", "w");
+    FILE *Freel = fopen("vol_traitement.dat", "w");
+    if(Fmax == NULL || Fcapt == NULL || Freel == NULL){
+        printf("erreur ouverture fichiers histogramme");
+        exit(1);
+    }
+    fprintf(Fmax,  "id;volume_max\n");
+    fprintf(Fcapt,  "id;volume_captation\n");
+    fprintf(Freel, "id;volume_traitement\n");
+    remplir_fichiers(a, Fmax, Fcapt, Freel);
+    fclose(Fmax);
+    fclose(Fcapt);
+    fclose(Freel);
+}
