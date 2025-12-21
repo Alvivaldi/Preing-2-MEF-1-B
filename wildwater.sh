5ee7944e57fc8f7d9c26f4370aa097633638d026
Lasugaa <lasugaa.k@gmail.com>
2025-12-21 00:08:50 +0000

    deplacement shell

--- wildwater.sh
+++ wildwater.sh
@@ -4,11 +4,6 @@ set -e
 # Mesure du temps
 START=$(date +%s)
 
-# Affichage du temps total
-END=$(date +%s)
-DURATION=$((END - START))
-
-echo "Durée totale du script : ${DURATION} secondes"
 
 #Verification de la compilation make 
 if [ ! -f "./wildwater" ]; then
@@ -26,3 +21,10 @@ if [ $RET -ne 0 ]; then
     echo "Erreur dans l'exécution du programme C !"
     exit 1
 fi
+
+
+# Affichage du temps total
+END=$(date +%s)
+DURATION=$((END - START))
+
+echo "Durée totale du script : ${DURATION} secondes"
